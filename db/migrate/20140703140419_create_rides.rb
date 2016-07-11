a54cdcc877b9606342ed726414ef9947785b367c
cassaram09 <cassaram09@gmail.com>
2016-07-11 14:47:25 -0400

    added migrations, completed user, ride, and attraction model tests. edited attaction test to include password for user creation

--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -1,3 +1,8 @@
 class CreateRides < ActiveRecord::Migration
-  # Write your migrations here
+  def change
+    create_table :rides do |t|
+      t.belongs_to :attraction
+      t.belongs_to :user
+    end
+  end
 end
